80bdd1976d4f3925c8a7b904461efeb99d57ff54
JackyLee <39452500+JackyLeeGM@users.noreply.github.com>
2023-10-26 15:29:12 +0800

    Update diy-part2.sh

--- config/Lienol-master/diy-part2.sh
+++ config/Lienol-master/diy-part2.sh
@@ -8,6 +8,8 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
@@ -16,14 +18,21 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='Lienol'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/sbwml/luci-app-alist package/alist
+svn co https://github.com/sbwml/luci-app-openai package/openai
+svn co https://github.com/esirplayground/luci-app-poweroff package/poweroff
+svn co https://github.com/sirpdboy/luci-app-parentcontrol package/parentcontrol
+svn co https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+svn co https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
+svn co https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
